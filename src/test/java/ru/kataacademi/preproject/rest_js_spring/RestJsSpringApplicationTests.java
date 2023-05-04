f5999ceef8b2c492ed4dd09c3996a21a323c58af
Alexander Punkov <alkpankov@gmail.com>
2023-05-04 11:30:10 +0300

    UtilCommit

---
+++ src/test/java/ru/kataacademi/preproject/rest_js_spring/RestJsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kataacademi.preproject.rest_js_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
