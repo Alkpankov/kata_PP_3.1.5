0f100f61eedfbd7f74cda9d045090022848ed4ec
Alexander Punkov <alkpankov@gmail.com>
2023-05-04 11:28:23 +0300

    FirstCommit

---
+++ src/main/java/ru/kataacademi/preproject/rest_js_spring/RestJsSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.kataacademi.preproject.rest_js_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJsSpringApplication.class, args);
+	}
+
+}
